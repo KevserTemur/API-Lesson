d27864847d9015e24fa81953ef7441404bef21a6
KevserTemur <kevsertemur@gmail.com>
2023-02-28 22:51:45 +0300

    api get

--- src/test/java/getRequest/get03.java
+++ src/test/java/getRequest/get03.java
@@ -1,29 +1,31 @@
 package getRequest;
 
 import baseURLs.JsonPlaceHolderBaseURL;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import org.testng.annotations.Test;
-
+import org.junit.Test;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class get03 extends JsonPlaceHolderBaseURL {
 
-    /*
-        Given
-            https://jsonplaceholder.typicode.com/todos/2
-        When
-             Kullanıcı GET Methodu ile Request Gönderir
-        Then
-            Status Code un "200" olduğunu Assert et
-		And
-            Content Type ın "application/json" olduğunu assert et
-		And
-		    title ın “quis ut nam facilis et officia qui” olduğunu verify et.,
-		And
-		    “completed” ın false olduğunu verify et.
-		And
-		    “userId” in 1 olduğunu verify et
-     */
+
+        /*
+                Given
+                    https://jsonplaceholder.typicode.com/todos/2
+                When
+                     Kullanıcı GET Methodu ile Request Gönderir
+                Then
+                    Status Code un "200" olduğunu Assert et
+                And
+                    Content Type ın "application/json" olduğunu assert et
+                And
+                    title ın “quis ut nam facilis et officia qui” olduğunu verify et.,
+                And
+                    “completed” ın false olduğunu verify et.
+                And
+                    “userId” in 1 olduğunu verify et
+             */
 
         /*
         {
@@ -35,7 +37,7 @@ public class get03 extends JsonPlaceHolderBaseURL {
          */
 
     @Test
-    public void get03() {
+    public void get03(){
 
        /*
        1) Set URL
@@ -45,14 +47,56 @@ public class get03 extends JsonPlaceHolderBaseURL {
         */
 
         // Step 1: Set URL:  https://jsonplaceholder.typicode.com/todos/2
-        specification.pathParams("todospath", "todos", "idPath", "2");
+        specification.pathParams("todosPath","todos",
+                "idPath","2");
 
         // Step 2: Set the Expected Data (ignored)
 
-        //Step 3: Send Request
-        Response response = given().spec(specification).when().get("/{todosPath}/{idPath}");
+       //Step 3: Send Request
+
+        Response response = given().
+                spec(specification).
+                when().
+                get("/{todosPath}/{idPath}");
+
         response.prettyPrint(); // like syso
 
         // Step 4: Assertion operations
+
+        /*
+         Then
+            Status Code un "200" olduğunu Assert et
+		And
+            Content Type ın "application/json" olduğunu assert et
+		And
+		    title ın “quis ut nam facilis et officia qui” olduğunu verify et.,
+		And
+		    “completed” ın false olduğunu verify et.
+		And
+		    “userId” in 1 olduğunu verify et
+         */
+
+        // 1. Way
+
+        /*
+        response.then().assertThat().statusCode(200).contentType("application/json");
+
+        response.
+                then().
+                assertThat().
+                body("title", Matchers.equalTo("quis ut nam facilis et officia qui"),
+                        "completed",Matchers.equalTo(false),
+                        "userId",Matchers.equalTo(1));*/
+
+        // 2. Way
+        response.
+                then().
+                assertThat().
+                statusCode(200).
+                contentType(ContentType.JSON).
+                body("title", equalTo("quis ut nam facilis et officia qui"),
+                        "completed",equalTo(false),
+                        "userId",equalTo(1));
+
     }
 }
