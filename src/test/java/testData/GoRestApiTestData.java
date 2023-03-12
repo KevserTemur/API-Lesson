51a410eddbf6f08ffd078946f5175067792eb1c1
KevserTemur <kevsertemur@gmail.com>
2023-03-12 12:48:22 +0300

    api

--- src/test/java/testData/GoRestApiTestData.java
+++ src/test/java/testData/GoRestApiTestData.java
@@ -4,24 +4,25 @@ import java.util.HashMap;
 
 public class GoRestApiTestData {
 
+
     //422
     public int statusCodeForPostInvalid(){
 
         return 422;
     }
 
-        
-        /*
-         {
-      "name": "Tenali Ramakrishna",
-      "gender": "male",
-      "email": "tenali.ramakrishna@15ce.com",
-      "status": "active"
-    }
-         */
-        
-        
-        
+
+    /*
+     {
+  "name": "Tenali Ramakrishna",
+  "gender": "male",
+  "email": "tenali.ramakrishna@15ce.com",
+  "status": "active"
+}
+     */
+
+
+
     public HashMap<String,String > reqBodyAndExpectedData(String name, String gender, String email, String status ){
 
         HashMap<String ,String > reqBodyAndExpectedDataMap = new HashMap<>();
@@ -34,4 +35,6 @@ public class GoRestApiTestData {
 
         return reqBodyAndExpectedDataMap;
     }
+
+
 }
