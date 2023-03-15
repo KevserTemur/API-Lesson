dbd92ffe3915ad692ac9fcba97b3425f932d5413
KevserTemur <kevsertemur@gmail.com>
2023-03-15 09:07:26 +0300

    api post

--- src/test/java/postRequest/Post03.java
+++ src/test/java/postRequest/Post03.java
@@ -12,39 +12,39 @@ import static org.junit.Assert.assertEquals;
 
 public class Post03 extends DummyRestApiBaseURL {
 
-        
-        /*
 
-  Given
+          /*
 
-      //http://dummy.restapiexample.com/api/v1/create
-  And
-      Request Body olarak aşağıdaki pattern de body gönderiniz
-    {
-"name": "Drake F.",
-"salary": "40000",
-"age": "27"
+    Given
+
+        //http://dummy.restapiexample.com/api/v1/create
+    And
+        Request Body olarak aşağıdaki pattern de body gönderiniz
+      {
+  "name": "Drake F.",
+  "salary": "40000",
+  "age": "27"
 }
-  When
-      Kullanıcı POST Methodu ile Request Gönderir
-  Then
-      Status Code un "200" olduğunu Assert et
-  And
-         Response Body nin aşağıdaki şekilde olduğunu doğrulayınız
-         {
-  "status": "success",
-  "data": {
-      "name": "Drake F.",
-      "salary": "40000",
-      "age": "27",
+    When
+        Kullanıcı POST Methodu ile Request Gönderir
+    Then
+        Status Code un "200" olduğunu Assert et
+    And
+           Response Body nin aşağıdaki şekilde olduğunu doğrulayınız
+           {
+    "status": "success",
+    "data": {
+        "name": "Drake F.",
+        "salary": "40000",
+        "age": "27",
 
-  },
-  "message": "Successfully! Record has been added."
+    },
+    "message": "Successfully! Record has been added."
 }
 
- */
-        
-        
+   */
+
+
     @Test
     public void post03(){
 
@@ -87,7 +87,7 @@ public class Post03 extends DummyRestApiBaseURL {
         //Basic Auth.  ---> kullanılaak.
         // Dokumanda suth istemiyor fakat biz örnek olsun diye nasıl kukllanıalcğaını öğrenmek amaçlı olarak basic auth. ile işlem yapacağız.
 
-        // ---> Beare token     Header da vermiştik.
+        // ---> Bearer token     Header da vermiştik.
         Response response = given().
                 spec(specification).
                 contentType(ContentType.JSON).
@@ -103,17 +103,32 @@ public class Post03 extends DummyRestApiBaseURL {
 
         //Step 4: Assertion İşlemi:
 
-        DummyRestApiExPojo actualData = response.as(DummyRestApiExPojo.class);
+        DummyRestApiExPojo actualData = response.as(DummyRestApiExPojo.class);  //POJO
         System.out.println("Actual Data: " + actualData);
 
+        assertEquals(dummyResTApiExPojo.getMessage(),actualData.getMessage());
+        assertEquals(dummyResTApiExPojo.getStatus(),actualData.getStatus());
+        assertEquals(dummyResTApiExPojo.getData().getSalary(),actualData.getData().getSalary());
+
+        /*-
         assertEquals(dummyResTApiExPojo.getStatus(),actualData.getStatus());
         assertEquals(dummyResTApiExPojo.getMessage(),actualData.getMessage());
         assertEquals(dummyResTApiExPojo.getData().getAge(),actualData.getData().getAge());
         assertEquals(dummyResTApiExPojo.getData().getName(),actualData.getData().getName());
         assertEquals(dummyResTApiExPojo.getData().getSalary(),actualData.getData().getSalary());
 
+         */
+
+
+
+
+
+
+
+
 
 
     }
 
-    }
+
+}
