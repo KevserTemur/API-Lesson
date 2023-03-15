5e68325da0e236f1f7300f4ad3e6958fc25788d7
KevserTemur <kevsertemur@gmail.com>
2023-03-15 09:08:02 +0300

    api put

--- src/test/java/putRequest/Put01.java
+++ src/test/java/putRequest/Put01.java
@@ -13,28 +13,28 @@ import static org.junit.Assert.assertEquals;
 
 public class Put01 extends JsonPlaceHolderBaseURL {
 
-        /*
-           Given
-               1) https://jsonplaceholder.typicode.com/todos/198
-               2) {
-                    "userId": 01,
-                    "title": "CW FSQA API Tutorial",
-                    "completed": true
-                  }
-           When
-                Kullanıcı Put Reques ile request gönderir
-           Then
-                 Status code is 200
-                Response body nin aşağıdaki gibi olduğunu verify eder.
-                  {
-                    "userId": 01,
-                    "title": "CW FSQA API Tutorial",
-                    "completed": true
-                  }
-        */
-        
-        
-        
+     /*
+        Given
+	        1) https://jsonplaceholder.typicode.com/todos/198
+	        2) {
+                 "userId": 01,
+                 "title": "CW FSQA API Tutorial",
+                 "completed": true
+               }
+        When
+	 		Kullanıcı Put Reques ile request gönderir
+	    Then
+	   	   Status code is 200
+	   	  Response body nin aşağıdaki gibi olduğunu verify eder.
+	   	    {
+                 "userId": 01,
+                 "title": "CW FSQA API Tutorial",
+                 "completed": true
+               }
+     */
+
+
+
     @Test
     public void put01(){
 
